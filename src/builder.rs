5f8f15ede379b702d6a76b66e0ebaf3fd13862f7
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2017-04-22 08:50:30 -0700

    adding load_value to the builder.

--- src/builder.rs
+++ src/builder.rs
@@ -22,7 +22,7 @@ impl FunctionBuilder {
         }
     }
 
-    pub fn allocate_local(&mut self, typ: Type) -> BuildObject {
+    pub fn allocate_local(&mut self, typ: &Type) -> BuildObject {
         self.registers.push(typ.clone());
         return BuildObject {
             typ: typ.clone(), register: self.registers.len() - 1
@@ -37,6 +37,17 @@ impl FunctionBuilder {
         }
         self.return_type = Some(obj.typ.clone());
         self.ops.push(Op::Return{register: obj.register});
+    }
+
+    pub fn load_value(&mut self, typ: &Type, value: i64) -> BuildObject {
+        let local = self.allocate_local(typ);
+        match typ {
+            &Type::Int => {
+                self.ops.push(Op::IntLoad{register: local.register, constant: value});
+            },
+            _ => {panic!("support todo")}
+        }
+        return local;
     }
 
     pub fn build(&mut self) -> Function {
